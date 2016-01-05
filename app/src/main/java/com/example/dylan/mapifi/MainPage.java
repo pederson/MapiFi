04610cfcec9160488b7a7a92de337a5454c15197
pederson <dpederson@utexas.edu>
2016-01-04 23:14:15 -0600

    Wifi strength now updates automatically using a Handler

--- app/src/main/java/com/example/dylan/mapifi/MainPage.java
+++ app/src/main/java/com/example/dylan/mapifi/MainPage.java
@@ -2,43 +2,80 @@ package com.example.dylan.mapifi;
 
 import android.content.Context;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainPage extends ActionBarActivity {
+    protected WifiManager mainWifi;
+    protected WifiInfo winfo;
+    protected TextView strength_textview;
+    protected LocationManager locationManager;
+    protected boolean update_status;
+    private android.os.Handler strengthHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        update_status = false;
+        strength_textview = (TextView)findViewById(R.id.strength_text);
         mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         winfo = mainWifi.getConnectionInfo();
+        update_status = false;
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        strengthHandler = new Handler();
     }
 
-    protected void onUpdateButton(View view){
+    protected Runnable mStrengthChecker = new Runnable() {
+        @Override
+        public void run() {
+            updateStrength();
+            strengthHandler.postDelayed(this, 100);
+        }
+    };
+
+    protected void startWifiUpdates() {
+        mStrengthChecker.run();
+    }
+
+    protected void stopWifiUpdates() {
+        strengthHandler.removeCallbacks(mStrengthChecker);
+    }
+
+    protected void startLocationUpdates() {
+
+    }
+
+    protected void stopLocationUpdates() {
+
+    }
+
+
+    protected void onRunButton(View view){
         // toggle status
         update_status = !update_status;
 
         // get run button text
         TextView t = (TextView)findViewById(R.id.run_button);
 
-        updateStrength();
-//        while(update_status) {
-//            t.setText("Stop");
-//            updateStrength();
-//        }
+        if (update_status){
+            t.setText("Stop");
+            startWifiUpdates();
+            startLocationUpdates();
+        }
+        else{
+            t.setText("Run");
+            stopWifiUpdates();
+            stopLocationUpdates();
+        }
 
-
-        t.setText("Run");
     }
 
     protected void updateStrength(){
@@ -48,14 +85,13 @@ public class MainPage extends ActionBarActivity {
 
         // if not connected to network...
         if (str > 100){
-            TextView t = (TextView)findViewById(R.id.strength_text);
-            t.setText("Wifi Unavailable");
+
+            strength_textview.setText("Wifi Unavailable");
             return;
         }
 
         // display the strength to user
-        TextView t = (TextView)findViewById(R.id.strength_text);
-        t.setText(String.valueOf(str));
+        strength_textview.setText(String.valueOf(str));
     }
 
     /*
@@ -65,6 +101,7 @@ public class MainPage extends ActionBarActivity {
      */
     protected int getStrength(){
         // check the Wifi state
+        //return 100;
         int state = mainWifi.getWifiState();
         if (state != WifiManager.WIFI_STATE_ENABLED){
             return 101;
@@ -78,8 +115,5 @@ public class MainPage extends ActionBarActivity {
 
     }
 
-    protected LocationManager locationManager;
-    protected WifiManager mainWifi;
-    protected WifiInfo winfo;
-    protected boolean update_status;
 }
+
